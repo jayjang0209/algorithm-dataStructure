13ecdbdbd22a57fe7c903f774928bdda38008e3a
Jay Jang <jayjang0209@gmail.com>
2022-12-19 14:46:57 -0800

    Add 21. merge two sorted lists solution

---
+++ linked_list/merge_two_sorted_list_21/merge_two_sorted_list_recur.py
@@ -0,0 +1,71 @@
+# Definition for singly-linked list.
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+
+
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        if list1 is None:
+            return list2
+
+        if list2 is None:
+            return list1
+
+        if list1.val <= list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
+
+    def print_list(self, head):
+        node = head
+        nodes = []
+        while node is not None:
+            nodes.append(str(node.val))
+            node = node.next
+        nodes.append("None")
+        return " -> ".join(nodes)
+
+
+a1 = ListNode(-10)
+a2 = ListNode(-9)
+a3 = ListNode(-6)
+a4 = ListNode(-4)
+a5 = ListNode(1)
+a6 = ListNode(9)
+a7 = ListNode(9)
+b1 = ListNode(-5)
+b2 = ListNode(-3)
+b3 = ListNode(0)
+b4 = ListNode(7)
+b5 = ListNode(8)
+b6 = ListNode(8)
+
+
+a1.next = a2
+a2.next = a3
+a3.next = a4
+a4.next = a5
+a5.next = a6
+a6.next = a7
+b1.next = b2
+b2.next = b3
+b3.next = b4
+b4.next = b5
+b5.next = b6
+
+s = Solution()
+ans = s.mergeTwoLists(a1, b1)
+print(s.print_list(ans))
+
